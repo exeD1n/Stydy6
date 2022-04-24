67f263bd687da49d1af2ea43b885d032d1a468ec
Daniil Lutaev <daniillutaev1@gmail.com>
2022-04-24 17:08:18 +0300

    gg

--- task3/task3.py
+++ task3/task3.py
@@ -1,4 +1,4 @@
-import csv
+from itertools import zip_longest
 
 def parsing_csv():
     user_open = open ('users.csv', 'r', encoding="utf-8")
@@ -6,28 +6,24 @@ def parsing_csv():
     
     user_array = []
     for us in user_open:
-        print(us.split(","))
+        user_array.append(us.replace(","," ").rstrip())
+    print(user_array)
     
     hobby_array = []
     for hb in hobby_open:
-        print([hb])
+        hobby_array.append(hb.rstrip())
+    print(hobby_array) 
+    
+    iterator = ((a,b) for a,b in zip_longest(user_array, hobby_array))
+    info = dict(iterator)
+    
+    with open ('info.txt', 'r+', encoding='UTF-8') as f:
+        f.write(str(info))
+    
     
     user_open.close()
     hobby_open.close()
     
-    
-    
-    
-    # with open ('users.csv', 'r', encoding="utf-8") as f:
-    #     reader = csv.reader(f)
-    #     print(reader)
-    #     for user in reader:
-    #         print(user)
-                    
-    # with open ('hobby.csv', 'r', encoding="utf-8") as f:
-    #     reader = csv.reader(f)
-    #     for line in reader:
-    #         print(line)
             
 if __name__ == "__main__":
     parsing_csv()
