18ce317f053546b536d006562eb8040a51861c60
Daniil Lutaev <daniillutaev1@gmail.com>
2022-04-26 10:18:10 +0300

    авы

--- task3/task3.py
+++ task3/task3.py
@@ -1,4 +1,5 @@
 from itertools import zip_longest
+import sys
 
 def parsing_csv():
     user_open = open ('users.csv', 'r', encoding="utf-8")
@@ -7,15 +8,18 @@ def parsing_csv():
     user_array = []
     for us in user_open:
         user_array.append(us.replace(","," ").rstrip())
-    print(user_array)
     
     hobby_array = []
     for hb in hobby_open:
         hobby_array.append(hb.rstrip())
-    print(hobby_array) 
     
-    iterator = ((a,b) for a,b in zip_longest(user_array, hobby_array))
-    info = dict(iterator)
+    if len(hobby_array) > len(user_array):
+        print("Ключей больше чем значений")
+        sys.exit()
+        
+    else:
+        iterator = ((a,b) for a,b in zip_longest(user_array, hobby_array))
+        info = dict(iterator)
     
     with open ('info.txt', 'r+', encoding='UTF-8') as f:
         f.write(str(info))
