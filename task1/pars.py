1d4211a14fe4f5e4913adefe7cb858ae8385bad2
Daniil Lutaev <daniillutaev1@gmail.com>
2022-04-26 09:59:06 +0300

    ваы

--- task1/pars.py
+++ task1/pars.py
@@ -1,16 +1,18 @@
-
 def parsing_txt():
-    fail = open('nginx_logs.txt', encoding='UTF-8')
+    array_info = []
+    
+    with open ('nginx_logs.txt', encoding='UTF-8') as f:
+        for line in f:
+            readline_file = line.split()
 
-    array_information = []
-    for line in fail.readlines():
-        remote_addr = line.split()[0]
-        request_type = line.split()[5].replace('"', "")
-        requested_resource = line.split()[6]
+            remote_addr = readline_file[0]
+            request_type = readline_file[5].replace('"', "")
+            requested_resource = readline_file[6]
 
-        result = (remote_addr, request_type, requested_resource)
-        array_information.append(result)
-    return array_information
+            result = (remote_addr, request_type, requested_resource)
+            array_info.append(result)
+        return array_info
+        
 
 if __name__ == "__main__":
     print(parsing_txt())
